09adfd0d983820f54f827d50a41ecfadabf97343
iliutaadrian <iliuta.podoleanu@golfgenius.com>
2026-02-24 10:24:06 +0200

    feat: diff view auto refresh

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto-reload files and refresh diffview when Neovim gains focus
+vim.api.nvim_create_autocmd("FocusGained", {
+  callback = function()
+    vim.cmd("checktime")
+    pcall(require("diffview.actions").refresh_files)
+  end,
+})
--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -3,8 +3,22 @@ return {
     "sindrets/diffview.nvim",
     cmd = { "DiffviewOpen", "DiffviewFileHistory" },
     keys = {
-      { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Diff View" },
-      { "<leader>gV", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+      { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Open Diff View" },
+      { "<leader>gV", "<cmd>DiffviewFileHistory %<cr>", desc = "Current File History" },
     },
+    config = function()
+      require("diffview").setup()
+
+      -- Auto-refresh diffview every 2 seconds when it's open
+      local timer = vim.uv.new_timer()
+      timer:start(
+        2000,
+        2000,
+        vim.schedule_wrap(function()
+          vim.cmd("checktime")
+          pcall(require("diffview.actions").refresh_files)
+        end)
+      )
+    end,
   },
 }
