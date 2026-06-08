72a5462388f0f64fe2e3b53b96cb98ad0cc20e81
iliutaadrian <iliuta.podoleanu@golfgenius.com>
2026-06-08 15:00:40 +0300

    feat: reveal file in Finder from snacks explorer with O

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -23,21 +23,69 @@ return {
       },
       picker = {
         hidden = true, -- include hidden files in all pickers
+        actions = {
+          reveal_in_finder = function(picker)
+            local item = picker:current()
+            if item and item.file then
+              vim.fn.jobstart({ "open", "-R", vim.fn.fnamemodify(item.file, ":p") }, { detach = true })
+            end
+          end,
+        },
         sources = {
           files = {
             hidden = true,
             ignored = true,
-            exclude = { ".git", ".devenv", "node_modules", ".direnv", ".idea", ".*cache.*", "*cache*", ".bundle", ".code-annotations", ".local_data" },
+            exclude = {
+              ".git",
+              ".devenv",
+              "node_modules",
+              ".direnv",
+              ".idea",
+              ".*cache.*",
+              "*cache*",
+              ".bundle",
+              ".code-annotations",
+              ".local_data",
+            },
           },
           grep = {
             hidden = true,
             ignored = true,
-            exclude = { ".git", ".devenv", "node_modules", ".direnv", ".idea", ".*cache.*", "*cache*", ".bundle", ".code-annotations", ".local_data" },
+            exclude = {
+              ".git",
+              ".devenv",
+              "node_modules",
+              ".direnv",
+              ".idea",
+              ".*cache.*",
+              "*cache*",
+              ".bundle",
+              ".code-annotations",
+              ".local_data",
+            },
           },
           explorer = {
             hidden = true,
             ignored = true,
-            exclude = { ".git", ".devenv", "node_modules", ".direnv", ".idea", ".*cache.*", "*cache*", ".bundle", ".code-annotations", ".local_data" },
+            exclude = {
+              ".git",
+              ".devenv",
+              "node_modules",
+              ".direnv",
+              ".idea",
+              ".*cache.*",
+              "*cache*",
+              ".bundle",
+              ".code-annotations",
+              ".local_data",
+            },
+            win = {
+              list = {
+                keys = {
+                  ["O"] = "reveal_in_finder",
+                },
+              },
+            },
           },
         },
       },
